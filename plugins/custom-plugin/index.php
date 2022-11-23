42f8ae2751e4a9e305a592b0ffd7f8717377d2e6
Damon Cook <damonacook@gmail.com>
2022-11-23 11:36:53 -0500

    👌 IMPROVE: Qorp to Custom theme/plugin

--- plugins/custom-plugin/index.php
+++ plugins/custom-plugin/index.php
@@ -1,5 +1,5 @@
 <?php
 /**
- * Plugin Name: Qorp plugin
+ * Plugin Name: Custom plugin
  * Description: Merely an empty plugin for demo purposes. This is where you could place custom plugin functionality.
  */
